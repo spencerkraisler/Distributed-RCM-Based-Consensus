fa1276b38ac0ded7d163daaf1b92b81c31edc0c0
Spencer Kraisler <32965911+spencerkraisler@users.noreply.github.com>
2023-08-24 12:09:58 -0700

    Add files via upload

---
+++ main.m
@@ -0,0 +1,52 @@
+clear; close all; clc
+
+%% Init constraints
+N = 10; % num agents
+tol = 1e-6; % tolerance for sim
+max_steps = 200; % max steps for sim
+
+n = 3;
+M = rotationsfactory(n); % SO(3) manifold object
+M.inner = @(x, xi, eta) trace(xi'*eta)/2;
+M.norm = @(x, xi) sqrt(M.inner(x, xi, xi));
+I = M.lie_identity();
+
+% Generate random connected undirected graph
+p = .4; % probability of 2 nodes being adjacent
+G = RandomConnectedGraph(N, p);
+
+% Generate init positions of agents
+sigma = pi/2; % st dev of gaussian distr
+center = M.rand(); % generate init agent positions around center
+x = repmat({}, 1, N);
+for i = 1:N
+    v_i = sigma*rand*M.randvec(I);
+    x_i = center*M.exp(I, v_i);
+    x{i} = x_i;
+end
+
+%% run sim
+consensus_error_hist = [];
+for t = 1:max_steps
+
+    % compute consensus error
+    consensus_error = ConsensusError(x, M, G); 
+    if consensus_error < tol % halt sim if tolerance achieved
+        break;
+    end
+    consensus_error_hist = [consensus_error_hist consensus_error];
+
+    i = mod(t, N) + 1;
+   
+    % compute RCM of nbrhood of agent i
+    N_i = neighbors(G, i)';
+    x{i} = RiemannianCenterOfMass(x(N_i), M); 
+end
+
+%% plot error over time
+hold on 
+grid on 
+set(gca, 'YScale', 'log'); % log y-scale
+xlabel('time step')
+ylabel('consensus error')
+plot(consensus_error_hist)
